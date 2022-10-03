87498bb526466fee93d4f8c17c35b087002ed34b
Hemedi Manyinja <hmanyinja@gmail.com>
2022-10-03 15:43:21 +0300

    DOne

--- routes/api.php
+++ routes/api.php
@@ -21,7 +21,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::any("incoming", function(Request $request){
     Log::error("INCOMING",$request->all());
-    HemmySendSms::send("255685639653",$request->TextBody);
+    $inbound = new \Postmark\Inbound($request->all());
+    HemmySendSms::send("255685639653",$inbound->Subject());
 })->name("incominng_sms");
 
 // 9f674c94b72cd6f424dd50e6ec0739f2@inbound.postmarkapp.com
