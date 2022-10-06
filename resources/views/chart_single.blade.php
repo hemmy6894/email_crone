a499549b927792fe15e20a0272316562aecf990c
Hemedi Manyinja <hmanyinja@gmail.com>
2022-10-06 20:09:55 +0300

    Done

--- resources/views/chart_single.blade.php
+++ resources/views/chart_single.blade.php
@@ -1,11 +1,10 @@
-@endforeach
 <div class="py-2 px-4 border-bottom d-none d-lg-block">
     <div class="d-flex align-items-center py-1">
         <div class="position-relative">
-            <img src="https://bootdey.com/img/Content/avatar/avatar3.png" class="rounded-circle mr-1" alt="Sharon Lessman" width="40" height="40">
+            <img src="https://ui-avatars.com/api/?name={{ $chart->OriginalMail }}" class="rounded-circle mr-1" alt="Sharon Lessman" width="40" height="40">
         </div>
         <div class="flex-grow-1 pl-3">
-            <strong>Sharon Lessman</strong>
+            <strong>{{ $chart->OriginalMail }}</strong>
         </div>
     </div>
 </div>
