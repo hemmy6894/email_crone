15b89205dd74a6e08b5503710c959412b945575f
Hemedi Manyinja <hmanyinja@gmail.com>
2022-10-06 20:17:35 +0300

    Done

--- resources/views/chart_single.blade.php
+++ resources/views/chart_single.blade.php
@@ -15,7 +15,7 @@
             @if($chart->MessageStream == "inbound")
                 <div class="chat-message-right mb-4">
                     <div>
-                        <img src="https://bootdey.com/img/Content/avatar/avatar1.png" class="rounded-circle mr-1" alt="Chris Wood" width="40" height="40">
+                        <img src="https://ui-avatars.com/api/?name={{ $chart->OriginalMail }}" class="rounded-circle mr-1" alt="Sharon Lessman" width="40" height="40">
                         <div class="text-muted small text-nowrap mt-2">{{ $chart->created_at->diffForHumans() }}</div>
                     </div>
                     <div class="flex-shrink-1 bg-light rounded py-2 px-3 mr-3">
@@ -27,7 +27,7 @@
             @if($chart->MessageStream == "outbound")
                 <div class="chat-message-left pb-4">
                     <div>
-                        <img src="https://ui-avatars.com/api/?name={{ $chart->OriginalMail }}" class="rounded-circle mr-1" alt="Sharon Lessman" width="40" height="40">
+                        <img src="https://bootdey.com/img/Content/avatar/avatar1.png" class="rounded-circle mr-1" alt="Chris Wood" width="40" height="40">
                         <div class="text-muted small text-nowrap mt-2">{{ $chart->created_at->diffForHumans() }}</div>
                     </div>
                     <div class="flex-shrink-1 bg-light rounded py-2 px-3 ml-3">
