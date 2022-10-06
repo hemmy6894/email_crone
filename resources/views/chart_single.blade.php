13bbf1158b84f5f7686daec934349aa6ac6fbdae
Hemedi Manyinja <hmanyinja@gmail.com>
2022-10-06 20:18:45 +0300

    Done

--- resources/views/chart_single.blade.php
+++ resources/views/chart_single.blade.php
@@ -13,25 +13,25 @@
     <div class="chat-messages p-4">
         @foreach($charts as $chart)
             @if($chart->MessageStream == "inbound")
-                <div class="chat-message-right mb-4">
+                <div class="chat-message-left mb-4">
                     <div>
                         <img src="https://ui-avatars.com/api/?name={{ $chart->OriginalMail }}" class="rounded-circle mr-1" alt="Sharon Lessman" width="40" height="40">
                         <div class="text-muted small text-nowrap mt-2">{{ $chart->created_at->diffForHumans() }}</div>
                     </div>
                     <div class="flex-shrink-1 bg-light rounded py-2 px-3 mr-3">
-                        <div class="font-weight-bold mb-1">You</div>
+                        <div class="font-weight-bold mb-1">{{ $chart->OriginalMail }}</div>
                         {!! nl2br($chart->TextBody) !!}
                     </div>
                 </div>
             @endif
             @if($chart->MessageStream == "outbound")
-                <div class="chat-message-left pb-4">
+                <div class="chat-message-right pb-4">
                     <div>
                         <img src="https://bootdey.com/img/Content/avatar/avatar1.png" class="rounded-circle mr-1" alt="Chris Wood" width="40" height="40">
                         <div class="text-muted small text-nowrap mt-2">{{ $chart->created_at->diffForHumans() }}</div>
                     </div>
                     <div class="flex-shrink-1 bg-light rounded py-2 px-3 ml-3">
-                        <div class="font-weight-bold mb-1">{{ $chart->OriginalMail }}</div>
+                        <div class="font-weight-bold mb-1">You</div>
                         {!! nl2br($chart->TextBody) !!}
                     </div>
                 </div>
