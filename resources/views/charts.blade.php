857617c1df862f1b928b4ab57278b2d0e2b4df93
Hemedi Manyinja <hmanyinja@gmail.com>
2022-10-07 09:26:17 +0300

    Done

--- resources/views/charts.blade.php
+++ resources/views/charts.blade.php
@@ -87,7 +87,7 @@
                                 <a href="#" class="list-group-item list-group-item-action border-0" onclick="clicked('{{ $url }}')">
                                     <div class="badge bg-success float-right">{{ $chart->total_mail }}</div>
                                     <div class="d-flex align-items-start">
-                                        <img src="https://ui-avatars.com/api/?name={{ $chart->OriginalMail }}" class="rounded-circle mr-1" alt="Vanessa Tucker" width="40" height="40">
+                                        <img src="https://ui-avatars.com/api/?name={{ $chart->OriginalMail }}" class="rounded-circle mr-1" alt="Vanessa Tucker" width="40" height="40">&nbsp;&nbsp;
                                         <div class="flex-grow-1 ml-3">
                                             {{ $chart->OriginalMail }}
                                             <div class="small"><span class="fas fa-circle chat-online"></span> Online</div>
