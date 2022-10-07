ad30a9f0e5c6a97c3ba14c5dabac3859534d66c5
Hemedi Manyinja <hmanyinja@gmail.com>
2022-10-07 09:29:49 +0300

    Done

--- resources/views/charts.blade.php
+++ resources/views/charts.blade.php
@@ -85,7 +85,7 @@
                             @if($chart->OriginalMail)
                                 @php($url = route("single_mail",["mail" => $chart->OriginalMail]))
                                 <a href="#" class="list-group-item list-group-item-action border-0" onclick="clicked('{{ $url }}')">
-                                    <div class="badge bg-success float-right">{{ $chart->total_mail }}</div>
+                                    <!-- <div class="badge bg-success float-right">{{ $chart->total_mail }}</div> -->
                                     <div class="d-flex align-items-start">
                                         <img src="https://ui-avatars.com/api/?name={{ $chart->OriginalMail }}" class="rounded-circle mr-1" alt="Vanessa Tucker" width="40" height="40">&nbsp;&nbsp;
                                         <div class="flex-grow-1 ml-3">
