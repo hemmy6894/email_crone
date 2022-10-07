4b12f22935f52dc374968d9ca2eb75b188d0d18f
Hemedi Manyinja <hmanyinja@gmail.com>
2022-10-07 11:39:39 +0300

    mails

--- resources/views/mails.blade.php
+++ resources/views/mails.blade.php
@@ -1,7 +1,7 @@
 @foreach($charts as $chart)
     @if($chart->OriginalMail)
         @php($url = route("single_mail",["mail" => $chart->OriginalMail]))
-        <a href="#" class="list-group-item list-group-item-action border-0 {{ $loop->first ? 'first_convo' : '' }}"  onclick="clicked('{{ $url }}')">
+        <a href="#" class="list-group-item list-group-item-action border-0 {{ $loop->first }} {{ $loop->first ? 'first_convo' : '' }}"  onclick="clicked('{{ $url }}')">
             <!-- <div class="badge bg-success float-right">{{ $chart->total_mail }}</div> -->
             <div class="d-flex align-items-start">
                 <img src="https://ui-avatars.com/api/?name={{ $chart->OriginalMail }}" class="rounded-circle mr-1" alt="Vanessa Tucker" width="40" height="40">&nbsp;&nbsp;
