e404f08ca15f5df0a0c06ffde44626b5c523e7f8
Hemedi Manyinja <hmanyinja@gmail.com>
2022-10-10 08:52:36 +0300

    Done

--- resources/views/mails.blade.php
+++ resources/views/mails.blade.php
@@ -6,7 +6,7 @@
         <a href="#" class="list-group-item list-group-item-action border-0 {{ $i == 1? 'first_convo' : '' }} class_{{ $chart->OriginalMail }}"  onclick="clicked('{{ $url }}')">
             <!-- <div class="badge bg-success float-right">{{ $chart->total_mail }}</div> -->
             <div class="d-flex align-items-start">
-                <img src="https://ui-avatars.com/api/?name={{ $chart->OriginalMail }}" class="rounded-circle mr-1" alt="Vanessa Tucker" width="40" height="40">&nbsp;&nbsp;
+                <img src="https://ui-avatars.com/api/?name={{ $chart->OriginalMail }}" class="rounded-circle mr-1" alt="Vanessa Tucker" width="40" height="40">
                 <div class="flex-grow-1 ml-3">
                     {{ $chart->OriginalMail }}
                     <div class="small"><span class="fas fa-circle chat-online"></span> Online</div>
