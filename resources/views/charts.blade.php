98c1ac9e4b320e2edaab0c1eefb578c192fa803e
Hemedi Manyinja <hmanyinja@gmail.com>
2022-10-10 08:49:26 +0300

    Done

--- resources/views/charts.blade.php
+++ resources/views/charts.blade.php
@@ -88,7 +88,7 @@
             </div>
         </div>
     </main>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+    <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script> -->
     <script>
         currentClicked = "";
 
